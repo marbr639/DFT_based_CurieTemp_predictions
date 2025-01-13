7f07377f24b0fac91df9e5b9c33ba1fedd4d3086
Marian <37578520+marbr639@users.noreply.github.com>
2025-01-13 11:57:13 +0100

    Create gather_mag_final.sh

---
+++ gather_mag_final.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rm magmom*
+
+#!/bin/bash
+
+# Check if the correct number of arguments were provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 Natoms outfile"
+    exit 1
+fi
+
+Natoms="$1"
+outfile="$2"
+
+
+echo "NATOMS: $NATOMS"
+echo "FILE: $FILE"
+
+
+
+b=$(( Natoms+3 ))
+
+
+	
+bzgrep -A $b 'magnetization (x)' $outfile | tail -$Natoms | awk '{print $5}' >mag_x
+bzgrep -A $b 'magnetization (y)' $outfile | tail -$Natoms | awk '{print $5}' >mag_y
+bzgrep -A $b 'magnetization (z)' $outfile | tail -$Natoms | awk '{print $5}' >mag_z
+paste mag_x mag_y mag_z >magmom
+
+awk '{print sqrt($1*$1+$2*$2+$3*$3)}' magmom >> magmom_size
+
+
+rm mag_*
+
+
