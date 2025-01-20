2b66af05356288522ba78be6148f694b3d5374b9
Marian <37578520+marbr639@users.noreply.github.com>
2025-01-20 12:47:36 +0100

    Update get_random_magnetic_configuration.py
    Correct size-settings

--- get_random_magnetic_configuration.py
+++ get_random_magnetic_configuration.py
@@ -1,15 +1,14 @@
 import sys
 import numpy as np
 
-if len(sys.argv) >= 3:
+if len(sys.argv) >= 0:
     input_lst = [float(i) for i in sys.argv[1:]]
     length=len(input_lst)
-    if (length % 2) == 0:
-        nAtoms = input_lst[:int(length/2)]
-        averageSizes = input_lst[int(length/2):]
+
+    nAtoms = input_lst[:length]
 
 c = {'config MAGMOM': []}
-for n, mag in zip(nAtoms, averageSizes):
+for n in nAtoms:
         for i in range(0,int(n)):
             point=np.array((0.0,0.0,0.0));
 
@@ -20,7 +19,7 @@ for n, mag in zip(nAtoms, averageSizes):
             while (np.sum(x2)>=1.0):
                 x=np.random.rand(4)*2-1;
                 x2=np.square(x);
-
+            mag = 3
             point[0]=2*(x[1]*x[3]+x[0]*x[2])/np.sum(x2);
             point[1]=2*(x[2]*x[3]-x[0]*x[1])/np.sum(x2);
             point[2]=(x2[0]+x2[3]-x2[1]-x2[2])/np.sum(x2);
